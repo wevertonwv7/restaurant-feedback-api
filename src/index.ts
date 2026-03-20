fca223168ffa06d54316f35c461f00e15df1d483
wevertonwv7 <wevitonsilva57@gmail.com>
2026-03-20 20:14:02 -0300

    config pra subir no railway

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,10 @@ import { birthdayWorker } from "./workers/birthdayWorker";
 import { processCampaigns } from "./modules/whatsapp/campaigns"; 
 import campaigns from "./routes/campaigns";
 import { campaignWorker } from "./workers/campaignWorker";
+import billing from "./routes/billing";
+import webhook from "./routes/webhook";
+
+
 
 const app = new Hono<{ Variables: Variables }>();
 
@@ -53,8 +57,8 @@ app.route("/api/attendant-ratings", attendantRatings);
 app.route("/api/reports", reports); 
 app.route("/api/whatsapp", whatsapp);
 app.route("/api/campaigns", campaigns);
-
-
+app.route("/api/billing", billing);
+app.route("/api/webhook", webhook);
 
 // 🚀 inicia worker sem quebrar o server
 whatsappWorker().catch((err) => {
@@ -70,8 +74,7 @@ whatsappWorker().catch((err) => {
 // 🚀 inicia worker sem quebrar o server
 campaignWorker().catch(console.error);
 
-const port = 3000;
-
+const port = Number(process.env.PORT) || 3000;
 console.log(`Servidor rodando em http://localhost:${port}`);
 
 serve({
---
+++ src/routes/webhook.ts
@@ -0,0 +1,43 @@
+import { Hono } from "hono";
+import { stripe } from "../lib/stripe";
+import { pool } from "../db/client";
+
+const app = new Hono();
+
+app.post("/", async (c) => {
+  const sig = c.req.header("stripe-signature")!;
+  const body = await c.req.text();
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (err) {
+    return c.text("Webhook error", 400);
+  }
+
+  // 🎯 PAGAMENTO APROVADO
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object as any;
+
+    const restaurantId = session.metadata.restaurantId;
+
+    // 🔥 ATIVA PLANO NO BANCO
+    await pool.query(
+      `
+      UPDATE restaurants
+      SET plan = 'pro'
+      WHERE id = $1
+      `,
+      [restaurantId]
+    );
+  }
+
+  return c.text("ok");
+});
+
+export default app;
