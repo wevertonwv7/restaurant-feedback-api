ce68916faa05c81b2091fb4ff8ec4192c06ef77b
wevertonwv7 <wevitonsilva57@gmail.com>
2026-03-20 22:23:14 -0300

    ajuste pra ver a db url

--- src/db/client.ts
+++ src/db/client.ts
@@ -2,6 +2,8 @@ import pkg from "pg";
 
 const { Pool } = pkg;
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL
+  
 });
