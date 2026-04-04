3b61b07810972c3009010125bc04e5a9097baf30
wevertonwv7 <wevitonsilva57@gmail.com>
2026-04-04 17:57:51 -0300

    Restrict CORS to the production frontend origin

--- src/index.ts
+++ src/index.ts
@@ -37,15 +37,7 @@ const app = new Hono<{ Variables: Variables }>();
 
 app.use(
   cors({
-    origin: [
-      "http://opiniofeedbacks.com",
-      "https://opiniofeedbacks.com",
-      "https://feedbacks-flow-dev.netlify.app",
-      "https://restaurant-feedback-api-dev.up.railway.app",
-      "http://localhost:3000",
-      "http://localhost:5173",
-      "http://localhost:8080"
-    ],
+    origin: ["https://opiniofeedbacks.com"],
     allowHeaders: ["Content-Type", "Authorization"],
     allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
